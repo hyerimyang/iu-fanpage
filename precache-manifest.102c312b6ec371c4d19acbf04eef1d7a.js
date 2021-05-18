f740eb6e5e9f18439a1d808487f8cabc5ebe537d
hyerimyang <did3296@naver.com>
2021-05-19 04:00:48 +0900

    Updates

---
+++ precache-manifest.102c312b6ec371c4d19acbf04eef1d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edaef0db102ae9b08457e03647857559",
+    "url": "/iu-fanpage/index.html"
+  },
+  {
+    "revision": "85217833a06b9b27a8ed",
+    "url": "/iu-fanpage/static/css/main.861dde52.chunk.css"
+  },
+  {
+    "revision": "676c51dc5914ce6e55f2",
+    "url": "/iu-fanpage/static/js/2.e9af0f79.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/iu-fanpage/static/js/2.e9af0f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85217833a06b9b27a8ed",
+    "url": "/iu-fanpage/static/js/main.ac6fc7e5.chunk.js"
+  },
+  {
+    "revision": "deb4399653367589cf27",
+    "url": "/iu-fanpage/static/js/runtime-main.e2bafd1a.js"
+  }
+]);
